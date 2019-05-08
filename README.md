6b805a72b675b552c58539ef215a4904434201b5
Jeff Tian <jie.tian@unicareer.com>
2019-05-08 15:26:51 +0800

    update resources

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu)
 [![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
 
-[![Build Status](https://travis-ci.org/Jeff-Tian/wechat-oauth-redirect.svg?branch=master)](https://travis-ci.org/Jeff-Tian/wechat-oauth-redirect)
+[![Build Status](https://travis-ci.com/fudan-mse/oms-frontend.svg?branch=master)](https://travis-ci.com/fudan-mse/oms-frontend)
 
 ### 解构此库：
 
--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -18,10 +18,36 @@ export default class SiteFooter extends React.Component {
                 Tushare.org
               </a>
             </p>
+            <p>
+              <a
+                href="https://www.useit.com.cn/thread-10573-1-1.html"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                证券交易系统架构设计
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://github.com/vnatesh/VWAP-Prediction"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                VWAP
+              </a>
+            </p>
           </Col>
           <Col span={6}>
-            <h3>Community</h3>
-            <p>To Do</p>
+            <h3>Features</h3>
+            <p>
+              <a
+                href="https://www.evernote.com/l/AGo96kMBMD5CoZhX86s7zMtBFVPxDfWHoaM"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                金融信息系统
+              </a>
+            </p>
           </Col>
           <Col span={6}>
             <h3>Help</h3>
