4ed816b87c68cdea040777a7b125687c35bbe0fc
Jeff Tian <jeff.tian@outlook.com>
2019-05-31 14:05:49 +0800

    fix: dev

--- server.js
+++ server.js
@@ -9,35 +9,44 @@ const app = express();
 const config = require("./webpack.dev.js");
 const compiler = webpack(config);
 
+app.use(function (req, res, next) {
+    console.log('url = ', req.url)
+
+    if (req.url.startsWith('/lib')) {
+        return res.sendFile(path.resolve(__dirname, req.url.replace('/lib', 'node_modules')))
+    }
+    next()
+})
+
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-  })
+    webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath
+    })
 );
 
 app.use(require("webpack-hot-middleware")(compiler));
 
 app.use(express.static("src/site-pages"));
 
-app.use("*", function(req, res, next) {
-  const filename = path.join(compiler.outputPath, `index.html`);
+app.use("*", function (req, res, next) {
+    const filename = path.join(compiler.outputPath, `index.html`);
 
-  compiler.outputFileSystem.readFile(filename, function(err, result) {
-    if (err) {
-      console.log(fs.readdirSync(compiler.outputPath));
-      console.error(req.url, filename);
-      return next(err);
-    }
+    compiler.outputFileSystem.readFile(filename, function (err, result) {
+        if (err) {
+            console.log(fs.readdirSync(compiler.outputPath));
+            console.error(req.url, filename);
+            return next(err);
+        }
 
-    res.set("content-type", "text/html");
-    res.send(result);
-    res.end();
-  });
+        res.set("content-type", "text/html");
+        res.send(result);
+        res.end();
+    });
 });
 
 // Serve the files on port 3000.
-app.listen(3000, function() {
-  console.log("app listening on port 3000!\n");
+app.listen(3000, function () {
+    console.log("app listening on port 3000!\n");
 });
