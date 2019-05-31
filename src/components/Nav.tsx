e84d5fbfcb45e2d79ee0917c63e8c580f1a29df6
Jeff Tian <jeff.tian@outlook.com>
2019-05-31 16:52:02 +0800

    fix: default path

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,31 +1,33 @@
 import * as React from "react";
-import { Menu } from "antd";
-import { Link } from "react-router-dom";
-export interface NavProps {}
+import {Menu} from "antd";
+import {Link} from "react-router-dom";
+
+export interface NavProps {
+}
 
 export const Nav = (props: NavProps) => (
-  <Menu
-    mode="horizontal"
-    theme="dark"
-    style={{ lineHeight: "64px" }}
-    defaultSelectedKeys={[
-      location.pathname.replace(process.env.ASSET_PATH, "/")
-    ]}
-  >
-    <Menu.Item key="/en">
-      <Link to={"/en"}>Market</Link>
-    </Menu.Item>
-    <Menu.Item key="/en/orders">
-      <Link to={"/en/orders"}>Orders</Link>
-    </Menu.Item>
-    <Menu.Item key="/source">
-      <a
-        href="https://github.com/fudan-mse/qbit"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Source
-      </a>
-    </Menu.Item>
-  </Menu>
+    <Menu
+        mode="horizontal"
+        theme="dark"
+        style={{lineHeight: "64px"}}
+        defaultSelectedKeys={[
+            location.pathname.replace(process.env.ASSET_PATH, "/").replace(/\/$/, '')
+        ]}
+    >
+        <Menu.Item key="/en">
+            <Link to={"/en"}>Market</Link>
+        </Menu.Item>
+        <Menu.Item key="/en/orders">
+            <Link to={"/en/orders"}>Orders</Link>
+        </Menu.Item>
+        <Menu.Item key="/source">
+            <a
+                href="https://github.com/fudan-mse/qbit"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Source
+            </a>
+        </Menu.Item>
+    </Menu>
 );
