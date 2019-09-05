9d722db0ac911345f1805a94ae8ceff00433c686
Jeff Tian <jie.tian@unicareer.com>
2019-09-05 19:17:16 +0800

    feat: 花旗账号登录

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,33 +1,43 @@
 import * as React from "react";
-import {Menu} from "antd";
-import {Link} from "react-router-dom";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
 
-export interface NavProps {
-}
+export interface NavProps {}
 
 export const Nav = (props: NavProps) => (
-    <Menu
-        mode="horizontal"
-        theme="dark"
-        style={{lineHeight: "64px"}}
-        defaultSelectedKeys={[
-            location.pathname.replace(process.env.ASSET_PATH, "/").replace(/\/$/, '')
-        ]}
-    >
-        <Menu.Item key="/en">
-            <Link to={"/en"}>Market</Link>
-        </Menu.Item>
-        <Menu.Item key="/en/orders">
-            <Link to={"/en/orders"}>Orders</Link>
-        </Menu.Item>
-        <Menu.Item key="/source">
-            <a
-                href="https://github.com/fudan-mse/qbit"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Source
-            </a>
-        </Menu.Item>
-    </Menu>
+  <Menu
+    mode="horizontal"
+    theme="dark"
+    style={{ lineHeight: "64px" }}
+    defaultSelectedKeys={[
+      location.pathname.replace(process.env.ASSET_PATH, "/").replace(/\/$/, "")
+    ]}
+  >
+    <Menu.Item key="/en">
+      <Link to={"/en"}>Market</Link>
+    </Menu.Item>
+    <Menu.Item key="/en/orders">
+      <Link to={"/en/orders"}>Orders</Link>
+    </Menu.Item>
+    <Menu.Item key="/source">
+      <a
+        href="https://github.com/fudan-mse/qbit"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Source
+      </a>
+    </Menu.Item>
+    <Menu.Item key="/login">
+      <a
+        href={
+          "https://uniheart.herokuapp.com/passport/citi?redirect_uri=" +
+          encodeURIComponent(location.href)
+        }
+        target="_self"
+      >
+        Sign In
+      </a>
+    </Menu.Item>
+  </Menu>
 );
